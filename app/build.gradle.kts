31286772ff7571ca6ea5288c71c7fef7e1319958
joeloewi7178 <joeloewi7178@gmail.com>
2022-05-22 00:59:33 +0900

    versionCode 8 versionName 1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.joeloewi.croissant"
         minSdk = 21
         targetSdk = 32
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
