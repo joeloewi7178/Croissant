403d90c4e76c896d6473c3e743e4e65374999aed
joeloewi7178 <joeloewi7178@gmail.com>
2022-06-10 01:05:16 +0900

    mod: cancel periodic check-in work when app updated

--- app/src/main/java/com/joeloewi/croissant/receiver/AlarmReceiver.kt
+++ app/src/main/java/com/joeloewi/croissant/receiver/AlarmReceiver.kt
@@ -42,6 +42,9 @@ class AlarmReceiver : BroadcastReceiver() {
                     getAllOneShotAttendanceUseCase().map { attendance ->
                         async {
                             attendance.runCatching {
+                                WorkManager.getInstance(application)
+                                    .cancelUniqueWork(attendance.attendCheckInEventWorkerName.toString())
+
                                 val alarmIntent =
                                     Intent(application, AlarmReceiver::class.java).apply {
                                         action = RECEIVE_ATTEND_CHECK_IN_ALARM
@@ -101,6 +104,9 @@ class AlarmReceiver : BroadcastReceiver() {
                 ) {
                     val attendanceWithGames = getOneAttendanceUseCase(attendanceId)
                     val attendance = attendanceWithGames.attendance
+
+                    WorkManager.getInstance(application)
+                        .cancelUniqueWork(attendance.attendCheckInEventWorkerName.toString())
 
                     val oneTimeWork = OneTimeWorkRequestBuilder<AttendCheckInEventWorker>()
                         .setInputData(workDataOf(AttendCheckInEventWorker.ATTENDANCE_ID to attendance.id))
