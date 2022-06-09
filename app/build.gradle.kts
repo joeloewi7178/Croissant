1584ab07894e54917ceb75b384094a7b97b8d921
joeloewi7178 <joeloewi7178@gmail.com>
2022-06-10 00:47:16 +0900

    versionCode 12 versionName 1.0.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.joeloewi.croissant"
         minSdk = 21
         targetSdk = 32
-        versionCode = 11
-        versionName = "1.0.11"
+        versionCode = 12
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
