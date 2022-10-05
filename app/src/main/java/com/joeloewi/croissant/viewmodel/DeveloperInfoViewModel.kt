58215a379798bfb41f6f223c613f9b22ddbc5880
joeloewi7178 <joeloewi7178@gmail.com>
2022-10-06 02:10:40 +0900

    mod: close tts service connection whenever flow was cancelled

--- app/src/main/java/com/joeloewi/croissant/viewmodel/DeveloperInfoViewModel.kt
+++ app/src/main/java/com/joeloewi/croissant/viewmodel/DeveloperInfoViewModel.kt
@@ -6,30 +6,34 @@ import androidx.lifecycle.viewModelScope
 import com.joeloewi.croissant.state.Lce
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
 import nl.marc_apps.tts.TextToSpeech
+import okhttp3.internal.closeQuietly
 import javax.inject.Inject
 
 @HiltViewModel
 class DeveloperInfoViewModel @Inject constructor(
     private val application: Application
 ) : ViewModel() {
-    val textToSpeech = flow {
-        emit(
-            TextToSpeech.runCatching {
-                createOrThrow(application)
-            }.fold(
-                onSuccess = {
-                    Lce.Content(it)
-                },
-                onFailure = {
-                    Lce.Error(it)
-                }
-            )
+    val textToSpeech = callbackFlow {
+        val textToSpeech = TextToSpeech.runCatching {
+            createOrThrow(application)
+        }.fold(
+            onSuccess = {
+                Lce.Content(it)
+            },
+            onFailure = {
+                Lce.Error(it)
+            }
         )
+
+        trySend(textToSpeech)
+
+        awaitClose { textToSpeech.content?.closeQuietly() }
     }.flowOn(Dispatchers.Default).stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(),
