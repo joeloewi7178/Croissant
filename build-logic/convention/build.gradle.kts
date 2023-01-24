6b92e3be655949c2dd040f4a50d2bea5a3ab5af9
joeloewi7178 <joeloewi7178@gmail.com>
2023-01-24 17:19:21 +0900

    add: jvmToolChain setting in gradle

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -4,9 +4,10 @@ plugins {
 
 group = "com.joeloewi.croissant.buildlogic"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(JavaVersion.VERSION_11.majorVersion))
+    }
 }
 
 dependencies {
