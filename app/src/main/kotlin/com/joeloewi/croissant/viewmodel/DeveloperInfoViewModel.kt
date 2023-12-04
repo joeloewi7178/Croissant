9cb220d0b6177a04ff2e148f5737be3069ee6f2b
joeloewi7178 <joeloewi7178@gmail.com>
2023-12-04 23:34:05 +0900

    mod: rebase

---
+++ app/src/main/kotlin/com/joeloewi/croissant/viewmodel/DeveloperInfoViewModel.kt
@@ -0,0 +1,43 @@
+package com.joeloewi.croissant.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.joeloewi.croissant.state.Lce
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.stateIn
+import nl.marc_apps.tts.TextToSpeechEngine
+import nl.marc_apps.tts.TextToSpeechFactory
+import okhttp3.internal.closeQuietly
+import javax.inject.Inject
+
+@HiltViewModel
+class DeveloperInfoViewModel @Inject constructor(
+    private val application: Application
+) : ViewModel() {
+    val textToSpeech = callbackFlow {
+        val textToSpeech = TextToSpeechFactory(application, TextToSpeechEngine.Google).runCatching {
+            createOrThrow()
+        }.fold(
+            onSuccess = {
+                Lce.Content(it)
+            },
+            onFailure = {
+                Lce.Error(it)
+            }
+        )
+
+        trySend(textToSpeech)
+
+        awaitClose { textToSpeech.content?.closeQuietly() }
+    }.flowOn(Dispatchers.Default).stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = Lce.Loading
+    )
+}
