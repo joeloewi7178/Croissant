ee972de33181ee1717e375f9b45d96a8af07e8ba
joeloewi7178 <joeloewi7178@gmail.com>
2024-07-09 13:45:27 +0900

    mod: pass language code properly when do genshin impact check-in

--- data/src/main/kotlin/com/joeloewi/croissant/data/api/dao/GenshinImpactCheckInService.kt
+++ data/src/main/kotlin/com/joeloewi/croissant/data/api/dao/GenshinImpactCheckInService.kt
@@ -5,16 +5,15 @@ import com.skydoves.sandwich.ApiResponse
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Query
 import java.util.Locale
 
 interface GenshinImpactCheckInService {
 
     @POST("event/sol/sign")
     suspend fun attend(
+        @Query("lang") language: String = Locale.getDefault().toLanguageTag().lowercase(),
         @Header("Cookie") cookie: String,
-        @Body params: Map<String, String> = mapOf(
-            "act_id" to "e202102251931481",
-            "lang" to Locale.getDefault().toLanguageTag().lowercase()
-        )
+        @Body params: Map<String, String> = mapOf("act_id" to "e202102251931481")
     ): ApiResponse<AttendanceResponse>
 }
